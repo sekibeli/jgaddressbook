03c38acc3fb59b240d2ac9a1cc47c4b2af3716fe
Julia Georgiew <java2011@me.com>
2023-06-18 15:14:45 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'jgaddressbook',
+    appId: '1:316455339794:web:8db76bea96ffdff41695cb',
+    storageBucket: 'jgaddressbook.appspot.com',
+    apiKey: 'AIzaSyBLlbgCOpNGApJHukSLF8UGelARnxcYVic',
+    authDomain: 'jgaddressbook.firebaseapp.com',
+    messagingSenderId: '316455339794',
+  },
+};
